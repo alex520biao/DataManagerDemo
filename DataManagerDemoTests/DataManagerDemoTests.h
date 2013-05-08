72f6729a1f34e91c9981b49174a8dec5fece983d
alex520biao <alex520biao@163.com>
2013-05-08 10:59:56 +0800

    Initial Commit

---
+++ DataManagerDemoTests/DataManagerDemoTests.h
@@ -0,0 +1,13 @@
+//
+//  DataManagerDemoTests.h
+//  DataManagerDemoTests
+//
+//  Created by alex on 13-5-8.
+//  Copyright (c) 2013年 alex. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface DataManagerDemoTests : SenTestCase
+
+@end
---
+++ DataManagerDemoTests/DataManagerDemoTests.m
@@ -0,0 +1,32 @@
+//
+//  DataManagerDemoTests.m
+//  DataManagerDemoTests
+//
+//  Created by alex on 13-5-8.
+//  Copyright (c) 2013年 alex. All rights reserved.
+//
+
+#import "DataManagerDemoTests.h"
+
+@implementation DataManagerDemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in DataManagerDemoTests");
+}
+
+@end
